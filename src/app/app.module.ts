e581dd889fe98eef56e1423237370a69e33b07f5
Oleksander <sanya953303@gmail.com>
2020-12-23 21:09:30 -0800

    add form and buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-<<<<<<< HEAD
-import { LoginComponent } from './components/login/login.component';
-=======
 import { RegistrationComponent } from './components/registration/registration.component';
->>>>>>> 8229c685b6387134718c9ef686731b316a5565a0
+import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    LoginComponent
-=======
+    LoginComponent,
     RegistrationComponent
->>>>>>> 8229c685b6387134718c9ef686731b316a5565a0
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
