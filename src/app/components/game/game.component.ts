d382cf5c89de8d226a91d4d86350efafbcd5e86c
Kevin Hake <kevin.hake@gmail.com>
2020-12-31 15:25:12 -0500

    adding some structure and http funcs to game

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { GameComponetService } from '../app/services/game/play.service';
+import { Word } from '../models/word';
+import { IncompleteStory } from '../models/incompletestory';
+import { CompleteStory } from '../models/completestory';
 
 @Component({
   selector: 'app-game',
--- src/app/services/game/play.service.ts
+++ src/app/services/game/play.service.ts
@@ -1,9 +1,34 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { CompleteStory } from 'src/app/models/complete-story/completestory';
+import { IncompleteStory } from 'src/app/models/incomplete-story/incompletestory';
+import { StoryCategory } from 'src/app/models/story-category/storycategory';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class PlayServiceService {
 
-  constructor() { }
+export class PlayService {
+
+  url="http://localhost:8083/madlibs/game";
+
+  constructor(private http: HttpClient) { }
+
+  getStory(category:StoryCategory): Observable<IncompleteStory> {
+    return this.http.get(`${this.url}/incomplete/${category}`) as Observable<IncompleteStory>;
+  }
+
+  saveCompleteStory(story:CompleteStory): Observable<CompleteStory> {
+    return this.http.post(`${this.url}/complete`, story) as Observable<CompleteStory>;
+  }
+
+  readStories(): Observable<CompleteStory[]> {
+    return this.http.get(`${this.url}/read`) as Observable<CompleteStory[]>;
+  }
+
+  rankStories(story:CompleteStory): Observable<CompleteStory> {
+    return this.http.patch(`${this.url}/updaterank`, story) as Observable<CompleteStory>;
+  }
 }
