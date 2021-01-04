a4b4ef75bbc92728eb30c8aac2ffb591b4de00c7
Oleksander <sanya953303@gmail.com>
2021-01-03 16:05:34 -0800

    work

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { LoginService } from 'src/app/services/login/login.service';
 import { Word } from 'src/app/models/word/word';
 import { WordCheckerService } from 'src/app/services/word-checker/word-checker.service';
+import { stringify } from '@angular/compiler/src/util';
+import { Noun } from 'src/app/models/word/noun';
+
 
 @Component({
   selector: 'app-login',
@@ -11,36 +14,59 @@ import { WordCheckerService } from 'src/app/services/word-checker/word-checker.s
 })
 export class LoginComponent implements OnInit {
 
-  public loginField:string = '';
-  public passwordField:string = '';
-  public result:string = '';
+  public loginField: string = '';
+  public passwordField: string = '';
+  public result: string = '';
+  public words:Word = null;
+  public nouns:Noun = new Noun();
 
-  constructor(private ls:LoginService, private wc:WordCheckerService) { }
+  constructor(private ls: LoginService, private wc: WordCheckerService, ) { }
 
   ngOnInit(): void {
-    // This's test code for checkWord function
-    this.wc.checkWord("tests").subscribe (
-      (data:Word)=>{
-        console.log(data);
-      },
-      ()=>{
-        console.log("something went wrong trying to get your word");
-      }
-    )
+
+    // This's return Noun
+    this.ls.getNoun('words');
+    
   }
 
-  checkLogin():void {
-    
-    this.ls.checkLoginBE(this.loginField, this.passwordField).subscribe (
-      (data:string) =>{
+  
+
+  checkLogin(): void {
+
+    this.ls.checkLoginBE(this.loginField, this.passwordField).subscribe(
+      (data: string) => {
         this.result = data;
         console.log("success");
       },
-      ()=>{
-        this.result="";
+      () => {
+        this.result = "";
         console.log("something went wrong checking your login and password.");
       }
     )
   }
+
+  // getNoun(word:string):void {
+  //   this.wc.checkWord(word).subscribe (
+  //     (data:Word)=>{
+  //       this.words = data;
+  //       for (var i in this.words) {
+  //         if (this.words[i].fl == "noun") { 
+  //           this.nouns.fl = this.words[i].fl;
+  //           if (this.words[i].sls) {
+  //             this.nouns.sls = "plural";
+  //             let regExp:RegExp = /\|([^|]+)\|/;
+  //             this.nouns.orig = regExp.exec(this.words[i].sls)[1];
+  //             console.log(this.nouns);
+  //           }
+            
+  //         }
+  //       }
+  //     },
+  //     ()=>{
+  //       this.words = null;
+  //       console.log("something went wrong trying to get your word");
+  //     }
+  //   )
+  // }
 
 }
