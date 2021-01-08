8b0d7d83d026541b38b9285c7c36ceca5a3f2494
Oleksander <sanya953303@gmail.com>
2021-01-08 10:27:03 -0800

    added read page

---
+++ src/app/models/completeStory3/complete-story3.spec.ts
@@ -0,0 +1,7 @@
+import { CompleteStory3 } from './complete-story3';
+
+describe('CompleteStory3', () => {
+  it('should create an instance', () => {
+    expect(new CompleteStory3()).toBeTruthy();
+  });
+});
